0f4b77a2849f676e7c922df8bb005b1cc730b981
Cristian Castellanos <cristiancastellanos@wposs.com>
2019-10-04 17:00:19 -0500

    Update main.dart
    Primer widget, contiene un card, text, icon, se modifca color y tamaño.

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+//void main() => runApp(MyApp());
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
 
 class MyApp extends StatelessWidget {
-  @override
   Widget build(BuildContext context) {
-
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Stateless widget"),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Card(
+                child: Container(
+                  padding: EdgeInsets.all(20.0),
+                    child: Column(
+              children: <Widget>[
+                Text(
+                  "I love flutter",
+                  style: TextStyle(color: Colors.grey, fontSize: 30.0),
+                ),
+                Icon(Icons.favorite,
+                  color: Colors.red,
+                  size: 40.0,)
+              ],
+            )))
+          ],
+        ),
+      ),
+    );
   }
 }
