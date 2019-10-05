d1b4816506bd081d8232a87080eeadda2b5e5b85
Cristian Castellanos <cristiancastellanos@wposs.com>
2019-10-05 07:49:25 -0500

    Update main.dart
    Se crea clase para reutilizar cards, se crean variables de clase evitanado duplicar código.

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -9,6 +9,10 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  final double iconSize = 40.0;
+  final TextStyle textStyle = TextStyle(color: Colors.grey, fontSize: 30.0);
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -19,23 +23,35 @@ class MyApp extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Card(
-                child: Container(
-                  padding: EdgeInsets.all(20.0),
-                    child: Column(
-              children: <Widget>[
-                Text(
-                  "I love flutter",
-                  style: TextStyle(color: Colors.grey, fontSize: 30.0),
-                ),
-                Icon(Icons.favorite,
-                  color: Colors.red,
-                  size: 40.0,)
-              ],
-            )))
+            MyCard(title: Text("I love flutter", style: textStyle),
+            icon: Icon(Icons.favorite, color: Colors.red , size: iconSize)),
+            MyCard(title: Text("I like this video", style: textStyle),
+                icon: Icon(Icons.thumb_up, color: Colors.blueAccent , size: iconSize)),
+            MyCard(title: Text("Next video", style: textStyle),
+                icon: Icon(Icons.queue_play_next, color: Colors.brown , size: iconSize))
           ],
         ),
       ),
     );
+  }
+}
+
+class MyCard extends StatelessWidget {
+  final Widget title;
+  final Widget icon;
+
+  MyCard({this.title,this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: Container(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              children: <Widget>[
+                this.title,
+                this.icon
+              ],
+            )));
   }
 }
